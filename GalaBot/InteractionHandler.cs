a3dae4a02c96f834220a70bbc4bd1667a03b727f
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-10-31 03:38:08 +0100

    Comenzando con el bot del servidor

---
+++ GalaBot/InteractionHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace GalaBot
+{
+    public class InteractionHandler
+    {
+        private readonly InteractionService _service;
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _provider;
+
+        public InteractionHandler(InteractionService service, DiscordSocketClient client, IServiceProvider provider)
+        {
+            _service = service;
+            _client = client;
+            _provider = provider;
+        }
+        public async Task InstallCommandsAsync()
+        {
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+            _client.InteractionCreated += HandleInteraction;
+            _service.SlashCommandExecuted += HandleSlashCommand;
+        }
+        private async Task HandleInteraction(SocketInteraction interaction)
+        {
+            try
+            {
+                SocketInteractionContext context = new(_client, interaction);
+                await _service.ExecuteCommandAsync(context, _provider);
+            }
+            catch (Exception e)
+            {
+                if (interaction.Type == InteractionType.ApplicationCommand)
+                    await interaction.RespondAsync("Ha ocurrido un error: " + e, ephemeral: true);
+            }
+        }
+        private async Task HandleServiceError(IInteractionContext context, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                if (result.Error == InteractionCommandError.UnknownCommand)
+                    return;
+
+                await context.Interaction.RespondAsync("Ha ocurrido un error: " + result.ErrorReason, ephemeral: true);
+            }
+        }
+        private async Task HandleSlashCommand(SlashCommandInfo info, IInteractionContext context, IResult result) =>
+            await HandleServiceError(context, result);
+    }
+}
---
+++ GalaBot/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Yaml;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GalaBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private SocketGuild? guild;
+
+        public SocketGuild Guild => guild ??= _client.Guilds.FirstOrDefault(g => g.Id == 985788875089793104)!;
+        
+        public static Program Instance;
+        
+        public static Task Main() => new Program().MainAsync();
+
+        public Program()
+        {
+            Instance = this;
+        }
+
+        public async Task MainAsync()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddYamlFile("config.yml")
+                .Build();
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                    services
+                        .AddSingleton(config)
+                        .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                        {
+                            GatewayIntents = GatewayIntents.All,
+                            AlwaysDownloadUsers = true,
+                            LogLevel = LogSeverity.Debug
+                        }))
+                        .AddMemoryCache()
+                        .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                        .AddSingleton<InteractionHandler>())
+                        .Build();
+            await RunAsync(host);
+        }
+
+        public async Task RunAsync(IHost host)
+        {
+            Console.WriteLine($"Iniciando Bot. Version: {Assembly.GetExecutingAssembly().GetName().Version}");
+            
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            
+            var commands = provider.GetRequiredService<InteractionService>();
+            _client = provider.GetRequiredService<DiscordSocketClient>();
+            var config = provider.GetRequiredService<IConfigurationRoot>();
+            
+            await provider.GetRequiredService<InteractionHandler>().InstallCommandsAsync();
+
+            _client.Ready += async () =>
+            {
+                await _client.SetStatusAsync(UserStatus.DoNotDisturb);
+                
+                Console.WriteLine($"Conectado como => {_client.CurrentUser}");
+                
+                if (IsDebug())
+                    await commands.RegisterCommandsToGuildAsync(UInt64.Parse(config["guild"]), true);
+                else
+                    await commands.RegisterCommandsGloballyAsync(true);
+            };
+            await _client.LoginAsync(TokenType.Bot, config["token"]);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        public static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
